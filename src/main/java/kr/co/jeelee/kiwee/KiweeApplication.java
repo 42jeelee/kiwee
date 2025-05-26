734f2863000cb67d408030a5b094a33d6d2c6a80
jeelee <>
2025-05-26 22:38:09 +0900

    Initial Code

---
+++ src/main/java/kr/co/jeelee/kiwee/KiweeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jeelee.kiwee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiweeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiweeApplication.class, args);
+	}
+
+}
