ff1e29bd9772ad8e6c1030f18a0d5f4691f40714
jeelee <>
2025-08-06 17:44:29 +0900

    Fix: setting CORS
    - 서버 CORS 설정
    
    - 엔드포인트 설계 다시 필요함

--- src/main/java/kr/co/jeelee/kiwee/global/config/SecurityConfig.java
+++ src/main/java/kr/co/jeelee/kiwee/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package kr.co.jeelee.kiwee.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -31,6 +32,7 @@ public class SecurityConfig {
 			.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
+			.cors(Customizer.withDefaults())
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(HttpMethod.GET, "/api/v1/platforms", "/api/v1/login/**").permitAll()
 				.requestMatchers(HttpMethod.POST, "/api/v1/tokens/refresh").permitAll()
