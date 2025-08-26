dfe8389908e0a33bcfc3b8da5bd433a3f979c4f8
jeelee <>
2025-08-26 17:40:25 +0900

    Style: rm import
    - 안쓰는 import문 제거
    
    - 엔드포인트 설계 다시 해야함

--- src/main/java/kr/co/jeelee/kiwee/domain/contentMember/service/ContentMemberServiceImpl.java
+++ src/main/java/kr/co/jeelee/kiwee/domain/contentMember/service/ContentMemberServiceImpl.java
@@ -8,8 +8,6 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
